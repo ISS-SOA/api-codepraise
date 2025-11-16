ISS-SOA/soa2024-api-codepraise

Restructure Monolith into Restful Web API
- Replace Presentation layer with Representation layer
- Replace Acceptance tests with API Acceptance tests
- Use ApiResult
- Use Representers

app/infrastructure/git/gateway/git_command.rb
@@ -41,7 +41,7 @@ module CodePraise
       end
 
       def options
-        @options.map { |option| "--#{option}" }
+        @options.map { "--#{_1}" }
       end
 
       def full_command

ISS-SOA/api-codepraise

app/infrastructure/git/gateway/git_command.rb
@@ -41,7 +41,7 @@ module CodePraise
       end
 
       def options
-        @options.map { |option| "--#{option}" }
+        @options.map { "--#{it}" }
       end
 
       def full_command